43c816aa0ea41ad2e75d1383c1878c28ebff2d0f
hhxx2015 <zhenyuan_hao@163.com>
2018-02-06 22:30:46 +0800

    文件路径

--- src/main/java/org/haohhxx/wechat/config/Config.java
+++ src/main/java/org/haohhxx/wechat/config/Config.java
@@ -11,6 +11,7 @@ import org.haohhxx.wechat.entity.SearchResult;
 import org.haohhxx.wechat.lucene.LuceneSearcher;
 
 import java.io.File;
+import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -25,17 +26,21 @@ public class Config {
 //
 //    public final static String disk_message_path = "/home/hao/文档/j2ee_workspace/diskSearcher/data/user_share/";
 
-    public final static String indexpath = "D:\\github\\wechat_dialog_server\\src\\main\\resources\\index";
-
+//    public final static String indexpath = ".\\index";
+//    public final static String indexpath = Config.class.getResource("/index/").getFile();
+//    public final static URL url = Config.class.getClassLoader().getResource("./index/");
+//    public final static String indexpath = new File(url.getFile()).getPath();
+    public static String indexpath = "/home/hao/index/";
     public static void main(String[] args) {
         ArrayList<SearchResult>  re = new ArrayList<>();
         try {
-            System.out.println(new File(indexpath).getPath());
+//            System.out.println(indexpath);
+//            System.out.println(new File(indexpath).getPath());
             int top=100;
             String field = "content";
             Analyzer analyzer = new StandardAnalyzer();
 
-            Path p = Paths.get(indexpath);
+            Path p = Paths.get(new File(indexpath).getPath());
             Directory dir = FSDirectory.open(p);
 
             IndexReader reader = DirectoryReader.open(dir);
---
+++ src/test/java/TestPost.java
@@ -0,0 +1,10 @@
+/**
+ * Created by admin on 2018/2/6.
+ */
+public class TestPost {
+
+    public static void main(String[] args) {
+
+    }
+
+}
