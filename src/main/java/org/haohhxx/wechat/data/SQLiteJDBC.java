08f3ce04c0f354e06ab350cbc0aa2d07e9612dcf
hhxx2015 <zhenyuan_hao@163.com>
2018-02-26 22:01:54 +0800

    sqlite

--- src/main/java/org/haohhxx/wechat/data/SQLiteJDBC.java
+++ src/main/java/org/haohhxx/wechat/data/SQLiteJDBC.java
@@ -1,16 +1,20 @@
 package org.haohhxx.wechat.data;
 
+import java.net.URL;
 import java.sql.*;
+
 /**
  * Created by admin on 2018/2/26.
  *
  */
 public class SQLiteJDBC {
     public static void main( String args[] ) {
+        URL url = SQLiteJDBC.class.getClassLoader().getResource("./db/");
+        String file = url.getPath();
         Connection c = null;
         try {
             Class.forName("org.sqlite.JDBC");
-            c = DriverManager.getConnection("jdbc:sqlite:test.db");
+            c = DriverManager.getConnection("jdbc:sqlite:"+file+"dialog.db");
         } catch ( Exception e ) {
             System.err.println( e.getClass().getName() + ": " + e.getMessage() );
             System.exit(0);
